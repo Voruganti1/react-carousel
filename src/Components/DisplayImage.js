83b115eb8adc661e1547e85eed39a682d30e4a93
Voruganti1 <sudhatvs@yahoo.com>
2022-10-18 20:08:54 +0100

    completed

--- src/Components/DisplayImage.js
+++ src/Components/DisplayImage.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useImages } from "./ImageInfo";
+import "./DisplayImage.css";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 function DisplayImage() {
-  const { image } = useImages();
-  return;
-  <>
-    {image &&
-      image.map((each, index) => {
-        return <img src={each.image} alt="Car models" />;
-      })}
-    ;
-  </>;
+  const [currant, setCurrant] = useState(0);
+  const { imageData } = useImages();
+  const length = imageData.length;
+  console.log(imageData);
+  const moveForward = () => {
+    setCurrant(currant === length - 1 ? 0 : currant + 1);
+  };
+  const moveBack = () => {
+    setCurrant(currant === 0 ? length - 1 : currant - 1);
+  };
+  return (
+    <>
+      <AiOutlineArrowLeft className="left" onClick={moveBack} />
+      <AiOutlineArrowRight className="right" onClick={moveForward} />
+      <div className="display">
+        {imageData &&
+          imageData.map((each, index) => {
+            return (
+              <div>
+                {index === currant && (
+                  <img
+                    className="image"
+                    src={each.image}
+                    alt="Car models"
+                    key={index}
+                  />
+                )}
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 }
 
 export default DisplayImage;
--- src/Components/ImageInfo.js
+++ src/Components/ImageInfo.js
@@ -1,9 +1,9 @@
 import React, { createContext, useContext } from "react";
-export const images = createContext();
+
 const imageData = [
   {
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcTeeExgl-L_gUpqXS3QEexczQsOxlizWSVQ&usqp=CAU",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTok0Eot0sRC3wh5LfKv5JUXWr7MgssH_--jw&usqp=CAU",
   },
   {
     image:
@@ -18,7 +18,9 @@ const imageData = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4y2a1G2Enm419EUdNHKuGWnn4mGY1n_t22Q&usqp=CAU",
   },
 ];
+export const images = createContext(undefined);
 function ImageInfo(props) {
+  console.log();
   return (
     <>
       <images.Provider value={{ imageData }}>{props.children}</images.Provider>
@@ -27,6 +29,7 @@ function ImageInfo(props) {
 }
 export function useImages() {
   const context = useContext(images);
+  console.log(context);
 
   if (!context) {
     throw new Error("There ate no images to show");
